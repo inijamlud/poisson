1870c44b6934bda8d3f5c006c611d6f90e01b7de
inijamlud <jamalude449@gmail.com>
2022-02-02 05:54:31 +0700

    update js

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-const e = 2.712;
-var fact=1;
+function poisson() {
+    
 
-const m = prompt("Masukkan rata-rata kota :");
-const x = prompt("Masukkan peluang yg dicari :");
+    const e = 2.712;
+    var fact = 1;
 
-function factorial (x){
-  if (x==0){
-    return 1;
-  } else {
-    for(var i=1;i<=x;i++){
-      fact=fact*i;    
-  }
-  return fact;
-  }
+    const m = document.getElementById("rata").value
+    const x = document.getElementById("peluang").value
+    var hasil = document.getElementById("hasil")
+
+
+    function factorial(x) {
+        if (x == 0) {
+            return 1;
+        } else {
+            for (var i = 1; i <= x; i++) {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+    
+    var p;
+    fact = factorial(x)
+    p = (Math.pow(e, -(m)) * Math.pow(m, x)) / fact;
+    hasil.value = p;
+    
+    // console.log("m=" + m)
+    // console.log("peluang yang didapat = " + p)
+
 }
-console.log("m="+ m)
-
-var p;
-fact = factorial(x)
-p = (Math.pow(e, -(m)) * Math.pow(m, x)) / fact;
-
-console.log("peluang yang didapat = "+p)
